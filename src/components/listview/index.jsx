019cb3453f0024942c4aed7b37b66c4952b74649
劳立营 <lly@senthink.com>
2020-05-18 17:40:50 +0800

    refactor: 格式化listView代码

--- src/components/listview/index.jsx
+++ src/components/listview/index.jsx
@@ -12,5 +12,5 @@ export default ListView;
  *  loadingMore: 是否加载完毕所有的数据，默认true
  *  loading: 加载更多数据请求是否进行中, 默认false
  *  refreshTip: 刷新tip, 传入一个object，属性值有pull, release, loading, refreshed
- *  bottomTip: 底部加载tip, 传入一个object，属性值有loading, loaded  
+ *  bottomTip: 底部加载tip, 传入一个object，属性值有loading, loaded
  */
--- src/components/listview/listview.jsx
+++ src/components/listview/listview.jsx
@@ -2,39 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './listview.less';
 
-const REFRESHTIP = { 
+const REFRESHTIP = {
   pull: '下拉刷新',
   release: '释放刷新',
   loading: '加载中...',
-  refreshed: '已刷新'
-}
+  refreshed: '已刷新',
+};
 
 const BOTTOMTIP = {
   loading: '正在加载中...',
-  loaded: '已经到底啦~'
-}
+  loaded: '已经到底啦~',
+};
 
-export default class ListView extends React.Component{
-  constructor (props) {
+export default class ListView extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       refreshProps: {
         pullHeight: 0,
         touchStart: 0,
         status: 'pull',
-      }
-    }
+      },
+    };
   }
 
   static defaultProps = {
     height: 400,
     refreshing: false,
-    refreshHandler: () => {}, 
+    refreshHandler: () => {},
     onReachBottom: () => {},
     loadingMore: true,
-    loading: false,  
+    loading: false,
     refreshTip: REFRESHTIP,
-    bottomTip: BOTTOMTIP,  
+    bottomTip: BOTTOMTIP,
   };
 
   static propTypes = {
@@ -50,15 +50,22 @@ export default class ListView extends React.Component{
 
   componentDidMount() {
     this.scroll.addEventListener('scroll', this.onReachBottom);
-    this.scroll.addEventListener('touchstart', this.onTouchStart, { passive: true });
-    this.scroll.addEventListener('touchmove', this.onTouchMove, { passive: false });
-    this.scroll.addEventListener('touchend', this.onTouchEnd, { passive: true });
+    this.scroll.addEventListener('touchstart', this.onTouchStart, {
+      passive: true,
+    });
+    this.scroll.addEventListener('touchmove', this.onTouchMove, {
+      passive: false,
+    });
+    this.scroll.addEventListener('touchend', this.onTouchEnd, {passive: true});
   }
 
   componentDidUpdate(prevProps) {
-    if (prevProps.refreshing !== this.props.refreshing && !this.props.refreshing) {
-      this.endRefreshFetching()
-    }    
+    if (
+      prevProps.refreshing !== this.props.refreshing &&
+      !this.props.refreshing
+    ) {
+      this.endRefreshFetching();
+    }
   }
 
   componentWillUnmount() {
@@ -68,17 +75,18 @@ export default class ListView extends React.Component{
     this.scroll.removeEventListener('touchend', this.onTouchEnd);
   }
 
-  onTouchStart = e => {
-    const { refreshProps } = this.state;
+  onTouchStart = (e) => {
+    const {refreshProps} = this.state;
     const touchStart = e.changedTouches[0].clientY;
-    this.setState({ refreshProps: { ...refreshProps, status: 'pull', touchStart },
-      scrollStart: this.scroll.scrollTop
+    this.setState({
+      refreshProps: {...refreshProps, status: 'pull', touchStart},
+      scrollStart: this.scroll.scrollTop,
     });
   };
 
-  onTouchMove = e => {
-    if(this.state.scrollStart === 0) {
-      const { refreshProps } = this.state;
+  onTouchMove = (e) => {
+    if (this.state.scrollStart === 0) {
+      const {refreshProps} = this.state;
       const lastestTop = e.changedTouches[0].clientY;
       let distance = lastestTop - refreshProps.touchStart;
       if (distance < 0) return;
@@ -91,66 +99,71 @@ export default class ListView extends React.Component{
           distance = 100;
         }
         refreshProps.pullHeight = distance;
-        this.setState({ refreshProps });
+        this.setState({refreshProps});
       }
     }
   };
 
   onTouchEnd = async () => {
-    const { refreshProps } = this.state;
+    const {refreshProps} = this.state;
     if (refreshProps.pullHeight > 80) {
       refreshProps.pullHeight = 40;
       refreshProps.status = 'loading';
-      this.setState({ refreshProps });
-      this.props.refreshHandler()
+      this.setState({refreshProps});
+      this.props.refreshHandler();
     } else {
       this.reset();
     }
   };
 
   endRefreshFetching = () => {
-    this.setState(({ refreshProps }) => { return (
-      { refreshProps: { ...refreshProps, pullHeight: 40, status: 'refreshed'} }
-    )});
-    setTimeout(this.reset, 1000)
-  }
+    this.setState(({refreshProps}) => {
+      return {
+        refreshProps: {...refreshProps, pullHeight: 40, status: 'refreshed'},
+      };
+    });
+    setTimeout(this.reset, 1000);
+  };
 
   reset = () => {
-    this.setState(({ refreshProps }) => { return (
-      { refreshProps: { ...refreshProps, pullHeight: 0, status: 'pull'} }
-    )}, );
-  }
+    this.setState(({refreshProps}) => {
+      return {refreshProps: {...refreshProps, pullHeight: 0, status: 'pull'}};
+    });
+  };
 
-  onReachBottom = e => {
-    const { clientHeight, scrollHeight, scrollTop } = e.target;
+  onReachBottom = (e) => {
+    const {clientHeight, scrollHeight, scrollTop} = e.target;
     const isBottom = scrollTop + clientHeight === scrollHeight;
-    const { loading, loadingMore } = this.props;
+    const {loading, loadingMore} = this.props;
     if (isBottom && loadingMore && !loading) {
       this.props.onReachBottom();
     }
   };
 
-  render () {
-    const { height, loadingMore, loading, refreshTip, bottomTip } = this.props;
-    const { refreshProps } = this.state;
-    const { pullHeight, status } = refreshProps;
+  render() {
+    const {height, loadingMore, loading, refreshTip, bottomTip} = this.props;
+    const {refreshProps} = this.state;
+    const {pullHeight, status} = refreshProps;
 
-    const combineRefreshTip = {...REFRESHTIP, ...refreshTip}
-    const combineBottomTip = {...BOTTOMTIP, ...bottomTip}
+    const combineRefreshTip = {...REFRESHTIP, ...refreshTip};
+    const combineBottomTip = {...BOTTOMTIP, ...bottomTip};
 
     return (
-      <div className="listview-wrap" style={{ height }} ref={e => this.scroll = e}>
-        <div className="refresh-wrap" style={{ height: pullHeight }}>
+      <div
+        className="listview-wrap"
+        style={{height}}
+        ref={(e) => (this.scroll = e)}>
+        <div className="refresh-wrap" style={{height: pullHeight}}>
           {combineRefreshTip[status]}
         </div>
         {this.props.children}
-        {loading && 
+        {loading && (
           <div className="bottom-tip">{combineBottomTip.loading}</div>
-        }
-        {!loading && !loadingMore && 
+        )}
+        {!loading && !loadingMore && (
           <div className="bottom-tip">{combineBottomTip.loaded}</div>
-        }
+        )}
       </div>
-    )
+    );
   }
 }
